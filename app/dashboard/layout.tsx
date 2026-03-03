fc80cb41849bb552d2d3cb8b209ebc25b4b4253f
Mark <71239113+MarkyMark25@users.noreply.github.com>
2026-03-03 20:19:45 +0800

    fix: made nav bar global

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,15 +1,3 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-white">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
-      </div>
-      
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12 bg-white text-zinc-800">
-        {children}
-      </div>
-    </div>
-  );
+  return <>{children}</>;
 }
