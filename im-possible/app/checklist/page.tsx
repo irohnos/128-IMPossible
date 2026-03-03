ddd5041f2d1faac497687d8dc31babf69b1222a9
sentimelista <159030785+sentimelista@users.noreply.github.com>
2026-03-03 15:53:08 +0800

    feat!: remove im-possible from branch

--- im-possible/app/checklist/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function StudentChecklistPage() {
-  return <div>Student Checklist</div>;
-}
--- im-possible/app/papers/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function StudentPapersPage() {
-  return <div>Student Papers</div>;
-}
